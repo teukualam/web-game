7de6497bc9c91c0acbcf475685a6f7646e14b7cc
Teuku Alam Faziansyah <teukualam@gmail.com>
2026-01-19 03:07:42 +0700

    Fix: Optimized vercel.json and cleaned build scripts

--- api/index.php
+++ api/index.php
@@ -1,2 +1,7 @@
 <?php
+
+// Jalankan pembersihan cache saat pertama kali diakses di server Vercel
+exec('php ../artisan config:cache');
+exec('php ../artisan view:cache');
+
 require __DIR__ . '/../public/index.php';
