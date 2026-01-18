040a6e43db6f90b575c1627cabbd87ad6b9898d7
Teuku Alam Faziansyah <teukualam@gmail.com>
2026-01-19 03:14:32 +0700

    Ultra Final Fix: Redirecting all Laravel caches to tmp

--- api/index.php
+++ api/index.php
@@ -1,7 +1,11 @@
 <?php
 
-// Paksa Laravel menggunakan folder /tmp untuk menyimpan view yang terkompilasi
+// Paksa Laravel menggunakan folder /tmp untuk semua file sementara
 putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
 
-// Impor file index Laravel yang asli
+// Memanggil file index Laravel yang asli
 require __DIR__ . '/../public/index.php';
